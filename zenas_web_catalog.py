934be06993b86d1893d09b06e4e13eefd7c898b2
Shiela Kristoffersen <46521426+shielakri@users.noreply.github.com>
2023-07-24 15:15:07 +0200

    Update zenas_web_catalog.py
    Checking connection to Snowflake

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -1,2 +1,19 @@
 import streamlit
 import snowflake.connector
+import pandas
+
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# Run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+# Put the data into a dataframe
+df = pandas.DataFrame(my_catalog)
+
+# Temp
+streamlit.write(df)
