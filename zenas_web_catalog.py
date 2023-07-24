341038b862a341d5afabdf2db764f27e04945cf5
Shiela Kristoffersen <46521426+shielakri@users.noreply.github.com>
2023-07-24 15:28:59 +0200

    Update zenas_web_catalog.py
    Get info from database based on the color picked

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -23,3 +23,20 @@ color_list = df[0].values.tolist()
 
 # Put a pick list for users to pick the color
 option = streamlit.selectbox('Pick a sweatsuit color or style:', color_list)
+
+# Build our product caption
+product_caption = 'Our warm, confortable,' + option + 'sweatsuit!'
+
+# Use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image(
+  df2[0],
+  width=400,
+  caption=product_caption
+)
+
+streamlit.write('Price:', df2[1])
+streamlit.write('Sizes Available', df2[2])
+streamlit.write(df2[3])
